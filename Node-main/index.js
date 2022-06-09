6cd4bd2afac86fb48754eb80ddaf70b4342520e9
Larissa Cardoso <106749260+larissacard@users.noreply.github.com>
2022-06-09 11:45:33 -0300

    Update index.js

--- Node-main/index.js
+++ Node-main/index.js
@@ -13,6 +13,6 @@ app.use(cors())
 app.use(routes)
 
 
-app.listen(21262, () => {
-    console.log(`Express started at http://localhost:21262`)
+app.listen(8080, () => {
+    console.log(`Express started at http://localhost:8080`)
 })
