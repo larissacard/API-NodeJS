69cc098879f3cdfd0f543ab172a5af90e3974ae0
Larissa Cardoso <106749260+larissacard@users.noreply.github.com>
2022-06-10 08:32:17 -0300

    Update routes.js

--- Node-main/config/routes.js
+++ Node-main/config/routes.js
@@ -2,36 +2,52 @@ const express = require('express')
 const routes = express.Router()
 
 let pessoas = [
-    {'1':{ Nome: 'Larissa', Profissao: 'frontEnd', Tarefas: 'tar'} },
-    {'2':{ Nome: 'Dayanne', Profissao: 'BackEnd', Tarefas: 'tar'} },
-    {'3':{ Nome: 'Iara', Profissao: 'BD', Tarefas: 'tar'} }
+    {Id:'1', Nome: 'Larissa', Profissao: 'frontEnd', Tarefas: 'tar' },
+    {Id:'2', Nome: 'Dayanne', Profissao: 'BackEnd', Tarefas: 'tar' },
+    {Id:'3', Nome: 'Iara', Profissao: 'BD', Tarefas: 'tar' }
 ]
 
 let projetos = [
-    {'1':{ Nome: 'Pousada', Equipe: 'Equipe 1', Tarefas: 'Tarefa 1'} },
-    {'2':{ Nome: 'API', Equipe: 'Equipe 2', Tarefas: 'Tarefa 1'} },
-    {'3':{ Nome: 'GP-Inovação', Equipe: 'Equipe 3', Tarefas: 'Tarefa 1'} }
+    {Id:'1', Nome: 'Pousada', Equipe: '1', Tarefas: '1' },
+    {Id:'2', Nome: 'API', Equipe: '2', Tarefas: '1'},
+    {Id:'3', Nome: 'GP-Inovação', Equipe: '3', Tarefas: '1'}
 ]
 
 let equipes = [
-    {'1':{ Nome: 'equipe 1', Membros: 'Array', Projetos: 'pro'} },
-    {'2':{ Nome: 'equipe 2', Membros: 'Array', Projetos: 'pro'} },
-    {'3':{ Nome: 'equipe 3', Membros: 'Array', Projetos: 'pro'} }
+    {Id:'1', Nome: 'equipe 1', Membros: 'Array', Projetos: '1'},
+    {Id:'2', Nome: 'equipe 2', Membros: 'Array', Projetos: '3'},
+    {Id:'3', Nome: 'equipe 3', Membros: 'Array', Projetos: '2'}
 ]
-routes.put('/:id', (req,res) =>{
-    const id = req.params.id
-    const Nome = req.body
-    pessoas[id] = Nome
-    
-    return res.json(pessoas)
-     })
 
-//GET -> selecionar
-routes.get('/', (req, res) => {
+let tarefas = [
+    {Id:'1', Nome: 'Task 1', Descrição: 'Descrição task 1', Projeto: '1'},
+    {Id:'2', Nome: 'Task 2', Descrição: 'Descrição task 1', Projeto: '2'},
+    {Id:'3', Nome: 'Task 3', Descrição: 'Descrição task 1', Projeto: '3'}
+]
+
+
+
+
+
+/* ------------------------------ Pessoas ------------------------------ */
+
+// Mostrando todas as pessoas
+routes.get('/pessoas', (req, res) => {
     return res.json(pessoas)
 })
-//POST -> inserir
-routes.post('/add', (req, res) => {
+
+// Mostrando pessoas com um ID específico
+routes.get('/pessoas/:id', (req, res) => {
+    const id = req.params.id
+    for (let index = 0; index < pessoas.length; index++) {
+        if (id == pessoas[index].Id) {
+            return res.json(pessoas[id-1])
+        }     
+    }
+})
+
+// Inserindo pessoas
+routes.post('/pessoas', (req, res) => {
     const body = req.body
 
     if (!body)
@@ -40,8 +56,9 @@ routes.post('/add', (req, res) => {
     pessoas.push(body)
     return res.json(body)
 })
-//DELETE -> deletar
-routes.delete('/:id', (req, res) => {
+
+// Deletando pessoas
+routes.delete('/pessoas/:id', (req, res) => {
     const id = req.params.id
 
     let newDB = pessoas.filter(item => { 
@@ -50,16 +67,40 @@ routes.delete('/:id', (req, res) => {
     })
 
     pessoas = newDB
-
     return res.send(newDB)
 })
 
-// Projetos
+// Editando pessoas
+routes.put('/pessoas/:id', (req,res) =>{
+    const id = req.params.id
+    pessoas[id-1] = req.body
+    
+    return res.json(pessoas)
+})
+
+
+
+
+
+/* ------------------------------ Projetos ------------------------------ */
+
+// Mostrando todos os Projetos
 routes.get('/projetos', (req, res) => {
     return res.json(projetos)
 })
-//POST -> inserir
-routes.post('/addProjetos', (req, res) => {
+
+// Mostrando projetos pelo ID
+routes.get('/projetos/:id', (req, res) => {
+    const id = req.params.id
+    for (let index = 0; index < projetos.length; index++) {
+        if (id == projetos[index].Id) {
+            return res.json(projetos[id-1])
+        }     
+    }
+})
+
+// Adicionando Projeto
+routes.post('/projetos', (req, res) => {
     const body = req.body
 
     if (!body)
@@ -68,8 +109,9 @@ routes.post('/addProjetos', (req, res) => {
     projetos.push(body)
     return res.json(body)
 })
-//DELETE -> deletar
-routes.delete('projetos/:id', (req, res) => {
+
+// Deletando Projeto
+routes.delete('/projetos/:id', (req, res) => {
     const id = req.params.id
 
     let newDB = projetos.filter(item => { 
@@ -78,16 +120,40 @@ routes.delete('projetos/:id', (req, res) => {
     })
 
     projetos = newDB
-
     return res.send(newDB)
 })
 
-// Equipe
+// Editando projeto
+routes.put('/projetos/:id', (req,res) =>{
+    const id = req.params.id
+    projetos[id-1] = req.body
+    
+    return res.json(projetos)
+})
+
+
+
+
+
+/* ------------------------------ Equipes ------------------------------ */
+
+// Mostrando todas as equipes
 routes.get('/equipes', (req, res) => {
     return res.json(equipes)
 })
-//POST -> inserir
-routes.post('equipes/add', (req, res) => {
+
+// Mostrando equipes específicas pelo ID
+routes.get('/equipes/:id', (req, res) => {
+    const id = req.params.id
+    for (let index = 0; index < equipes.length; index++) {
+        if (id == equipes[index].Id) {
+            return res.json(equipes[id-1])
+        }     
+    }
+})
+
+// Inserindo equipe 
+routes.post('/equipes', (req, res) => {
     const body = req.body
 
     if (!body)
@@ -96,8 +162,9 @@ routes.post('equipes/add', (req, res) => {
     equipes.push(body)
     return res.json(body)
 })
-//DELETE -> deletar
-routes.delete('equipes/:id', (req, res) => {
+
+// Deletando equipe pelo ID
+routes.delete('/equipes/:id', (req, res) => {
     const id = req.params.id
 
     let newDB = equipes.filter(item => { 
@@ -106,8 +173,77 @@ routes.delete('equipes/:id', (req, res) => {
     })
 
     equipes = newDB
-
     return res.send(newDB)
 })
 
+// Editando Equipe
+routes.put('/equipes/:id', (req,res) =>{
+    const id = req.params.id
+    equipes[id-1] = req.body
+    
+    return res.json(equipes)
+})
+
+
+/* ------------------------------ Tarefas ------------------------------ */
+
+// Mostrando todas as tarefas
+routes.get('/tarefas', (req, res) => {
+    return res.json(tarefas)
+})
+
+// Mostrando tarefas com um ID específico
+routes.get('/tarefas/:id', (req, res) => {
+    const id = req.params.id
+    for (let index = 0; index < tarefas.length; index++) {
+        if (id == tarefas[index].Id) {
+            return res.json(tarefas[id-1])
+        }     
+    }
+})
+
+// Inserindo tarefas
+routes.post('/tarefas', (req, res) => {
+    const body = req.body
+
+    if (!body)
+        return res.status(400).end()
+
+    tarefas.push(body)
+    return res.json(body)
+})
+
+// Deletando tarefas
+routes.delete('/tarefas/:id', (req, res) => {
+    const id = req.params.id
+
+    let newDB = tarefas.filter(item => { 
+        if (!item[id])
+            return item
+    })
+
+    pessoas = newDB
+    return res.send(newDB)
+})
+
+// Editando tarefas
+routes.put('/tarefas/:id', (req,res) =>{
+    const id = req.params.id
+    tarefas[id-1] = req.body
+    
+    return res.json(tarefas)
+})
+
+
 module.exports = routes
+
+
+
+
+
+
+/* -=-=-=-=-=-=-=-=- Mostrando a equipe de um projeto -=-=-=-=-=-=-=-= */
+routes.get('/projetos/equipes/:id', (req, res) => {
+    const id = req.params.id
+    return res.json(proj)
+})
